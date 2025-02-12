87702595f67015ea9d82dbc0530223b1efe3bb45
ujjwalbokde <143083146+ujjwalbokde@users.noreply.github.com>
2025-02-12 17:19:08 +0530

    font updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,19 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import { Inter, Roboto_Mono } from "next/font/google";
 
-const geistSans = Geist({
+
+const geistSans = Inter({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
+const geistMono = Roboto_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+
 
 export const metadata = {
   title: "Ujjwal Bokde",
